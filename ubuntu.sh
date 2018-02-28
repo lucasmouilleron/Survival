b344e878c2afb037c3677093e644df01425114c0
Lucas Mouilleron <lucas.mouilleron@me.com>
2018-02-28 17:39:06 +0100

    more

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,5 +4,5 @@ ghr="https://raw.githubusercontent.com/lucasmouilleron/Survival/master"
 
 sudo apt-get install -y curl git zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl  -O -L $ghr/configs/.zshrc
-curl  -O -L $ghr/configs/.hushlogin
+curl  -O -L -H "Cache-Control: no-cache" $ghr/configs/.zshrc
+curl  -O -L -H "Cache-Control: no-cache" $ghr/configs/.hushlogin
