f1030d7818db8973a021e06fbbf73be4fbfe5f9e
Lucas Mouilleron <lucas.mouilleron@me.com>
2018-02-28 17:54:01 +0100

    more

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,8 +8,10 @@ sudo apt-get update
 echo "Installing binaries ..."
 sudo apt-get install -y curl git zsh vim glances
 
-echo "Configurating ..."
+echo "Running install ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Configuring ..."
 curl  -O -L $ghr/configs/.zshrc
 curl  -O -L $ghr/configs/.vimrc
 curl  -O -L $ghr/configs/.hushlogin
