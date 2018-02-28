fc07da3e59dfbda543184d17da9e56903a018a8d
Lucas Mouilleron <lucas.mouilleron@me.com>
2018-02-28 18:46:28 +0100

    more

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,11 +4,33 @@
 GHR="https://raw.githubusercontent.com/lucasmouilleron/Survival/master"
 ##############################################################
 RED='\033[0;31m'
+GREEN='\033[0;33m'
+BLUE='\033[0;34m'
 NC='\033[0m'
 
 ##############################################################
 printStep() {
+    echo "$GREEN$1$NC"
+}
+
+##############################################################
+printError() {
     echo "$RED$1$NC"
+}
+
+##############################################################
+printSmallStep() {
+    echo "$BLUE$1$NC"
+}
+
+##############################################################
+getGHFile() {
+    printSmallStep "Downloading file $GHR/$1"
+    curl -O -sL --fail $GHR/$1
+    returnCode=$?
+    if [ "$returnCode" -ne "0" ]; then
+        printError "Can't download file $GHR/$1 $returnCode"
+    fi
 }
 
 ##############################################################
@@ -22,10 +44,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 ##############################################################
 printStep "Configuring ..."
-cd $HOME;curl -O -sL $GHR/configs/.zshrc
-cd $HOME;curl -O -sL $GHR/configs/.vimrc
-cd $HOME;curl -O -sL $GHR/configs/.selected_editor
-cd $HOME;curl -O -sL $GHR/configs/.hushlogin
+cd $HOME;getGHFile configs/.zshrc
+cd $HOME;getGHFile configs/.vimrc
+cd $HOME;getGHFile configs/.selected_editor
+cd $HOME;getGHFile configs/.hushlogin
 
 ##############################################################
 printStep "Done !"
