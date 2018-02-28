66071a93b74b948a51eeae84ee34196806cad43b
Lucas Mouilleron <lucas.mouilleron@me.com>
2018-02-28 17:55:56 +0100

    more

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,15 +3,15 @@
 ghr="https://raw.githubusercontent.com/lucasmouilleron/Survival/master"
 
 echo "Updating ..."
-sudo apt-get update
+sudo apt-get -qq update
 
 echo "Installing binaries ..."
-sudo apt-get install -y curl git zsh vim glances
+sudo apt-get install -qq -y curl git zsh vim glances
 
 echo "Running install ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Configuring ..."
-curl  -O -L $ghr/configs/.zshrc
-curl  -O -L $ghr/configs/.vimrc
-curl  -O -L $ghr/configs/.hushlogin
+curl  -O -sL $ghr/configs/.zshrc
+curl  -O -sL $ghr/configs/.vimrc
+curl  -O -sL $ghr/configs/.hushlogin
