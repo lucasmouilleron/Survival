b978b676fb4b4b24309bb4a8c7054ed1f05c2584
Lucas Mouilleron <lucas.mouilleron@me.com>
2018-03-03 10:02:26 +0100

    more

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,8 +6,6 @@
 VERSION="1.0"
 GHR="https://raw.githubusercontent.com/lucasmouilleron/Survival/master"
 
-https://github.com/lucasmouilleron/Survival/raw/master/configs/.config/micro/bindings.json
-
 ##############################################################
 # HELPERS
 ##############################################################
